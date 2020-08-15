a7bbc047555e264ce994506af6e1a14444349f6f
Christopher Escalon <escalonchristopher@gmail.com>
2020-08-14 22:34:58 -0600

    Basic project structure

--- server/src/Data/Contexts/TelemedicineContext.cs
+++ server/src/Data/Contexts/TelemedicineContext.cs
@@ -6,7 +6,6 @@ namespace Data.Contexts
     {
         public TelemedicineContext(DbContextOptions options) : base(options)
         {
-                
         }
     }
 }
---
+++ server/src/Domain/Aggregates/Patient.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Contracts;
+
+namespace Domain.Aggregates
+{
+    public class Patient : BaseEntity, IAggregateRoot
+    {
+        public string Name { get; set; }
+    }
+}
