c039de63566721c29404a5e4f866851f27c92fa8
Gerardo Gonzales <gerargd@outlook.es>
2020-08-14 21:24:38 -0600

    Server Project Inicialization

---
+++ server/src/Data/Contexts/TelemedicineContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts
+{
+    public class TelemedicineContext : DbContext
+    {
+        public TelemedicineContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+    }
+}
