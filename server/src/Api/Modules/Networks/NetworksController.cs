0f3c70b9ec101ce58ff78d364ecde86ca68f9239
Christopher Escalon <escalonchristopher@gmail.com>
2020-08-23 16:01:15 -0600

    validation of code

--- server/src/Api/Modules/Networks/NetworksController.cs
+++ server/src/Api/Modules/Networks/NetworksController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Modules.Networks
@@ -7,44 +8,39 @@ namespace Api.Modules.Networks
     [Route("[controller]")]
     public class NetworksController : Controller
     {
-        private readonly NetworkViewModel[] networks =
+        private readonly NetworkViewModel[] _networks =
         {
             new NetworkViewModel
             {
                 Id = 1,
                 Name = "El Progreso",
-                City = "El Progreso"
+                City = "El Progreso",
+                Code = 2121
             },
             new NetworkViewModel
             {
                 Id = 2,
                 Name = "SPS",
-                City = "San Pedro Sula"
+                City = "San Pedro Sula",
+                Code = 2020
             },
             new NetworkViewModel
             {
                 Id = 3,
                 Name = "La Ceiba",
-                City = "La Ceiba"
+                City = "La Ceiba",
+                Code = 3030
             }
         };
 
+
         // GET
-        [HttpGet()]
-        public IEnumerable<NetworkViewModel> Get()
+        [HttpGet("{id:int?}")]
+        public IEnumerable<NetworkViewModel> Get(int? id, [FromQuery] int? code)
         {
-            return networks;
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<NetworkViewModel> Get(int? id)
-        {
-            if (id == null)
-            {
-                return BadRequest("the id parameter can't be null");
-            }
-
-            return networks[id.Value - 1];
+            return _networks
+                .Where(x => id == null || x.Id == id)
+                .Where(x => code == null || x.Code == code);
         }
 
         //localhost:5000/networks
