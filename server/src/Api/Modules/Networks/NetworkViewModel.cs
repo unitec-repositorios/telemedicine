9f705c85279b3e00d815ffead82db7df3d710838
Christopher Escalon <escalonchristopher@gmail.com>
2020-08-23 16:01:15 -0600

    validation of code

--- server/src/Api/Modules/Networks/NetworkViewModel.cs
+++ server/src/Api/Modules/Networks/NetworkViewModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public string Code { get; set; }
+        public int Code { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
     }
