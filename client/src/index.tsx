f3b706bad4148b9c350bbcf324732038fd1e3130
Erasmo Santos <mitoo_95@unitec.edu>
2020-09-02 12:09:20 -0600

    Cambio en archivo index
    No funcionaba el app por una linea de codigo que se agrego al refactor el nombre de los archivos

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import "./containers/referenceForm/components/node_modules/antd/dist/antd.css";
-
 ReactDOM.render(<App />, document.getElementById("root"));
